07b4d28cbd2c6bb04c99533cafe11ef2d558f434
shohag <pkshohag240@gmail.com>
2023-06-17 01:57:44 +0600

    api not found route funciton added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import globalErrorHandelar from './app/middlewares/globalErrorHandelar'
 import routes from './app/routes'
+import httpStatus from 'http-status-codes'
 
 const app = express()
 
@@ -15,6 +16,21 @@ app.use(globalErrorHandelar)
 app.get('/', async (req: Request, res: Response) => {
   res.send('dynamic course auth server is running...')
   // Promise.reject(new Error('dynamic course auth server is running'))
+})
+
+// not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    sussecc: false,
+    message: 'Not Found',
+    errorMessage: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  })
+  next()
 })
 
 export default app
